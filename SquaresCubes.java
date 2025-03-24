6004644e38a9c140682d73bc892ecebaa78b963e
Hopeson-Jockiel <jockielh@gmail.com>
2025-03-24 18:17:14 +0100

    primitive type

---
+++ SquaresCubes.java
@@ -0,0 +1,8 @@
+public class SquaresCubes {
+    public static void main(String[] args) {
+        System.out.println("Number  Square  Cube");
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf("%d\t%d\t%d%n", i, i * i, i * i * i);
+        }
+    }
+}
