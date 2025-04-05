5cecbff66f2478086395723ef613369ea3d37406
Hopeson-Jockiel <jockielh@gmail.com>
2025-04-05 16:42:44 +0100

    Primitve type

--- SquaresCubes.java
+++ SquaresCubes.java
@@ -1,8 +1,46 @@
+import java.util.Scanner;
+
+
 public class SquaresCubes {
     public static void main(String[] args) {
-        System.out.println("Number  Square  Cube");
-        for (int i = 0; i <= 10; i++) {
-            System.out.printf("%d\t%d\t%d%n", i, i * i, i * i * i);
-        }
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println(" Enter Integers From 0 to 10\n ");
+		int num0 = input.nextInt();
+		int num1 = input.nextInt();
+		int num2 = input.nextInt();
+		int num3 = input.nextInt();
+		int num4 = input.nextInt();
+		int num5 = input.nextInt();
+		int num6 = input.nextInt();
+		int num7 = input.nextInt();
+		int num8 = input.nextInt();
+		int num9 = input.nextInt();
+		int num10 = input.nextInt();
+		
+		System.out.printf(" %d\t%d\t%d%n  ", num0, (num0 * num0), (num0 * num0 * num0));
+		System.out.printf(" %d\t%d\t%d%n  ", num1, (num1 * num1), (num1 * num1 * num1));
+		System.out.printf(" %d\t%d\t%d%n  ", num2, (num2 * num2), (num2 * num2 * num2));
+		System.out.printf(" %d\t%d\t%d%n  ", num3, (num3 * num3), (num3 * num3 * num3));
+		System.out.printf(" %d\t%d\t%d%n  ", num4, (num4 * num4), (num4 * num4 * num4));
+		System.out.printf(" %d\t%d\t%d%n  ", num5, (num5 * num5), (num5 * num5 * num5));
+		System.out.printf(" %d\t%d\t%d%n  ", num6, (num6 * num6), (num6 * num6 * num6));
+		System.out.printf(" %d\t%d\t%d%n  ", num7, (num7 * num7), (num7 * num7 * num7));
+		System.out.printf(" %d\t%d\t%d%n  ", num8, (num8 * num8), (num8 * num8 * num8));
+		System.out.printf(" %d\t%d\t%d%n  ", num9, (num9 * num9), (num9 * num9 * num9));
+		System.out.printf(" %d\t%d\t%d%n  ", num10, (num10 * num10), (num10 * num10 * num10));
+		
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
